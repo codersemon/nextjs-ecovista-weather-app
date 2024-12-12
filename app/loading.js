a71cc33efba9d3952be7c86da02aeebb2c073033
Emon Khan <codersemon@gmail.com>
2024-12-12 16:12:17 +0600

    loader file updated

--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,9 @@
 const RootLoader = () => {
-  return <div className="animate-bounce">Loading...</div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className="animate-bounce text-2xl">Loading...</h1>
+    </div>
+  );
 };
 
 export default RootLoader;
